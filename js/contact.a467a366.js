4af42f7ef7ebe4719e41ecbb1b806caed90276a1
YOGABAGAS\acer <yogabagas69@gmail.com>
2020-07-05 01:36:24 +0700

    first init

---
+++ js/contact.a467a366.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["contact"],{b8fa:function(t,n,e){"use strict";e.r(n);var c=function(){var t=this,n=t.$createElement;t._self._c;return t._m(0)},a=[function(){var t=this,n=t.$createElement,e=t._self._c||n;return e("div",{staticClass:"contact"},[e("h1",[t._v("This is an contact page")])])}],s=e("2877"),i={},l=Object(s["a"])(i,c,a,!1,null,null,null);n["default"]=l.exports}}]);
+//# sourceMappingURL=contact.a467a366.js.map
